d55d324726c90c0e2a19a3bed948f581b9b24ab1
Laraib <laraibazhar107@gmail.com>
2026-02-28 20:47:39 +0500

    new

--- script.js
+++ script.js
@@ -163,25 +163,15 @@ skillItems.forEach(item => {
     });
 });
 
-// Experience cards tilt effect
+// Experience cards - subtle lift
 const experienceCards = document.querySelectorAll('.experience-card, .cert-card');
 experienceCards.forEach(card => {
-    card.addEventListener('mousemove', function (e) {
-        const rect = this.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-
-        const rotateX = (y - centerY) / 10;
-        const rotateY = (centerX - x) / 10;
-
-        this.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-10px)`;
+    card.addEventListener('mouseenter', function () {
+        this.style.transform = 'translateY(-10px)';
     });
 
     card.addEventListener('mouseleave', function () {
-        this.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) translateY(0)';
+        this.style.transform = 'translateY(0)';
     });
 });
 
